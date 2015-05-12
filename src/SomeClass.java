43107fae595f0acffcc3dca6e25407ed3c58865f
sergeybelokon <sergey.belokon@smartexe.com>
2015-05-12 14:58:34 +0300

    Added SomeClass

---
+++ src/SomeClass.java
@@ -0,0 +1,46 @@
+/**
+ * Created by sbelokon on 5/12/15.
+ */
+public class SomeClass {
+    public int getSomeId() {
+        return someId;
+    }
+
+    public void setSomeId(int someId) {
+        this.someId = someId;
+    }
+
+    public int getSomeNumber() {
+        return someNumber;
+    }
+
+    public void setSomeNumber(int someNumber) {
+        this.someNumber = someNumber;
+    }
+
+    public int someId;
+    public int someNumber;
+
+    public SomeClass() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SomeClass)) return false;
+
+        SomeClass someClass = (SomeClass) o;
+
+        if (someId != someClass.someId) return false;
+        if (someNumber != someClass.someNumber) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = someId;
+        result = 31 * result + someNumber;
+        return result;
+    }
+}
